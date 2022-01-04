27ebffbafdf3bfb46939c814d9039a00663d1e0c
o.chuiko <o.chuiko@temabit.com>
2022-01-04 16:58:31 +0200

    fix

--- DependencyInjection/OzznestApmExtension.php
+++ DependencyInjection/OzznestApmExtension.php
@@ -1,8 +1,11 @@
 <?php
+namespace Ozznest\ApmBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
 
 class OzznestApmExtension extends Extension
 {
